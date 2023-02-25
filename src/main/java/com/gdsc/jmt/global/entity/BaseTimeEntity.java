27cda8110cce9f314f62d86aa5f85866ec3c9dac
boo105 <boo10500@naver.com>
2023-02-25 17:03:27 +0900

    JPA Auditing 제거

--- src/main/java/com/gdsc/jmt/global/entity/BaseTimeEntity.java
+++ src/main/java/com/gdsc/jmt/global/entity/BaseTimeEntity.java
@@ -1,21 +1,17 @@
 package com.gdsc.jmt.global.entity;
 
 import jakarta.persistence.Column;
-import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
 import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.UpdateTimestamp;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @MappedSuperclass
-@EntityListeners(AuditingEntityListener.class)
 public class BaseTimeEntity {
+    @CreationTimestamp
     @Column(name = "created_time", nullable = false, updatable = false, columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
     public LocalDateTime createdTime;
 
+//    @UpdateTimestamp
     @Column(name = "modified_time", nullable = false, updatable = true, columnDefinition = "TIMESTAMP ON UPDATE CURRENT_TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
     public LocalDateTime modifiedTime;
 }
