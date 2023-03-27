cb5bb627a534b08e1cdbc1b4f328cb6eb152e444
myeongju00 <j51677767@gmail.com>
2023-03-27 23:03:54 +0900

    refactor : 절대 경로 상대 경로로 변경

--- src/main/java/com/gdsc/jmt/domain/user/command/service/UserService.java
+++ src/main/java/com/gdsc/jmt/domain/user/command/service/UserService.java
@@ -6,6 +6,8 @@ import com.gdsc.jmt.global.exception.ApiException;
 import com.gdsc.jmt.global.messege.UserMessage;
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import lombok.RequiredArgsConstructor;
 import org.axonframework.commandhandling.gateway.CommandGateway;
 import org.springframework.stereotype.Service;
@@ -17,7 +19,7 @@ import org.springframework.web.multipart.MultipartFile;
 public class UserService {
     private final CommandGateway commandGateway;
     //TODO : 임시로 피오니 로컬로 테스트 해봤어요. 나중에는 s3로 바꿔야할 것 같아요.
-    private final String PROFILE_IMAGE_URL = "/Users/jeongmyeongju/github/jmt-backend/src/main/resources/image/";
+    private final String PROFILE_IMAGE_URL = "/src/main/resources/image/";
 
     @Transactional
     public void updateUserNickName(String userAggregateId, String nickName) {
@@ -34,16 +36,16 @@ public class UserService {
                 userAggregateId,
                 PROFILE_IMAGE_URL + profileImg.getOriginalFilename()
         ));
-        System.out.println("이미지 업로드 성공");
     }
 
     private void uploadImage(MultipartFile image){
         try {
             File file = new File(PROFILE_IMAGE_URL);
+            Path path = Paths.get("");
             if (!file.exists()) { // 파일이 존재하지 않는다면 디렉토리 생성
                 file.mkdirs();
             }
-            File destination = new File(PROFILE_IMAGE_URL + File.separator + image.getOriginalFilename());
+            File destination = new File(path.toAbsolutePath() + PROFILE_IMAGE_URL + File.separator + image.getOriginalFilename());
             image.transferTo(destination);
         } catch (IOException e) {
             throw new ApiException(UserMessage.PROFILE_IMAGE_UPLOAD_FAIL);
