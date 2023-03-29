bea6ef27215f549915739737baacc4c2d6156c9b
myeongju00 <j51677767@gmail.com>
2023-03-30 00:00:47 +0900

    feat : 유저 데이터 조회 API 스웨거 작성

--- src/main/java/com/gdsc/jmt/domain/user/query/controller/springdocs/getUserInfoSpringDocs.java
+++ src/main/java/com/gdsc/jmt/domain/user/query/controller/springdocs/getUserInfoSpringDocs.java
@@ -1,12 +1,12 @@
-package com.gdsc.jmt.domain.restaurant.query.controller.springdocs;
+package com.gdsc.jmt.domain.user.query.controller.springdocs;
 
 import com.gdsc.jmt.global.controller.springdocs.ServerErrorException;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
-
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -14,19 +14,21 @@ import java.lang.annotation.Target;
 
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
-@Operation(summary = "맛집 위치 정보 조회 API", description = "카카오 검색 API를 사용하여 맛집 위치 정보를 조회합니다.")
+@Operation(summary = "닉네임 중복 확인 API", description = "닉네임을 등록하기 전에 등록 가능한 닉네임인지 확인하는 API 입니다.")
+@Parameter(name = "nickname", description = "닉네임", required = true)
 @ApiResponses(value = {
         @ApiResponse(responseCode = "200", useReturnTypeSchema = true),
         @ApiResponse(
                 responseCode = "404",
-                description = "유저 정보를 찾을 수 없습니다.",
-                content = @Content(mediaType = "application/json", schema = @Schema(implementation = UserNotFoundException.class)
-        )
+                description = "이미 존재하는 닉네임 입니다.",
+                content = @Content(mediaType = "application/json", schema = @Schema(implementation = NicknameConflictErrorException.class))
+        ),
         @ApiResponse(
                 responseCode = "500",
                 description = "알수 없는 서버 에러 발생",
                 content = @Content(mediaType = "application/json", schema = @Schema(implementation = ServerErrorException.class))
         )
+
 })
-public @interface FindRestaurantLocationSpringDocs {
+public @interface getUserInfoSpringDocs {
 }
