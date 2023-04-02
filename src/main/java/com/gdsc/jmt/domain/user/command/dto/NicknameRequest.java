307b7482b2e66db754d31fef5b0fbff882c9619a
myeongju00 <j51677767@gmail.com>
2023-04-02 21:57:14 +0900

    fix : 사용자 인증 정보에 aggregateId 가 들어오지 않아 request로 받았던 부분 수정

--- src/main/java/com/gdsc/jmt/domain/user/command/dto/NicknameRequest.java
+++ src/main/java/com/gdsc/jmt/domain/user/command/dto/NicknameRequest.java
@@ -1,7 +1,5 @@
 package com.gdsc.jmt.domain.user.command.dto;
 
 public record NicknameRequest(
-        String userAggregateId,
         String nickname
-) {
-}
+) { }
--- src/main/java/com/gdsc/jmt/domain/user/command/dto/ProfileImgRequest.java
+++ src/main/java/com/gdsc/jmt/domain/user/command/dto/ProfileImgRequest.java
@@ -3,7 +3,5 @@ package com.gdsc.jmt.domain.user.command.dto;
 import org.springframework.web.multipart.MultipartFile;
 
 public record ProfileImgRequest(
-        String userAggregateId,
         MultipartFile profileImg
-) {
-}
+) { }
