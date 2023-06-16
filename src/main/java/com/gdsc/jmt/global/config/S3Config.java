7ff7813c079f6ee8a97d3200e5afa18d0a6179d9
myeongju00 <j51677767@gmail.com>
2023-06-16 16:48:33 +0900

    [feature/JMT-99] feat : S3 Config 오류 수정 및 파일 업로드 기능 구현

--- src/main/java/com/gdsc/jmt/global/config/S3Config.java
+++ src/main/java/com/gdsc/jmt/global/config/S3Config.java
@@ -1,12 +1,13 @@
 package com.gdsc.jmt.global.config;
 
-import com.amazonaws.auth.AWSStaticCredentialsProvider;
-import com.amazonaws.auth.BasicAWSCredentials;
-import com.amazonaws.services.s3.AmazonS3Client;
-import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import software.amazon.awssdk.auth.credentials.AwsCredentials;
+import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
 
 @Configuration
 public class S3Config {
@@ -18,11 +19,27 @@ public class S3Config {
     private String region;
 
     @Bean
-    public AmazonS3Client amazonS3Client() {
-        BasicAWSCredentials awsCredentials = new BasicAWSCredentials(accessKey, secretKey);
-        return (AmazonS3Client) AmazonS3ClientBuilder.standard()
-                .withRegion(region)
-                .withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
+    @Primary
+    public AwsCredentialsProvider customAwsCredentialsProvider() {
+        return () -> new AwsCredentials() {
+            @Override
+            public String accessKeyId() {
+                return accessKey;
+            }
+
+            @Override
+            public String secretAccessKey() {
+                return secretKey;
+            }
+        };
+    }
+
+    @Bean
+    @Primary
+    public S3Client s3Client() {
+        return S3Client.builder()
+                .credentialsProvider(customAwsCredentialsProvider())
+                .region(Region.of(region))
                 .build();
     }
 }
