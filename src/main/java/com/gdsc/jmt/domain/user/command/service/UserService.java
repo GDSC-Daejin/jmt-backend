d4b7ddd3e5667f4b11076d21f65c725cd8b69424
myeongju00 <j51677767@gmail.com>
2023-07-05 22:15:49 +0900

    [feature/JMT-103] feat : 불필요한 주석 제거

--- src/main/java/com/gdsc/jmt/domain/user/command/service/UserService.java
+++ src/main/java/com/gdsc/jmt/domain/user/command/service/UserService.java
@@ -5,10 +5,7 @@ import com.gdsc.jmt.domain.user.command.UpdateUserProfileImgCommand;
 import com.gdsc.jmt.global.exception.ApiException;
 import com.gdsc.jmt.global.messege.UserMessage;
 import com.gdsc.jmt.global.service.S3FileService;
-import java.io.File;
 import java.io.IOException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import lombok.RequiredArgsConstructor;
 import org.axonframework.commandhandling.gateway.CommandGateway;
 import org.springframework.stereotype.Service;
@@ -20,7 +17,7 @@ import org.springframework.web.multipart.MultipartFile;
 public class UserService {
     private final CommandGateway commandGateway;
     private final S3FileService s3FileService;
-    //TODO : 임시로 피오니 로컬로 테스트 해봤어요. 나중에는 s3로 바꿔야할 것 같아요.
+
     private final String DEFAULT_PROFILE_IMAGE_URL = "https://gdsc-jmt.s3.ap-northeast-2.amazonaws.com/profileImg/defaultImg/Default+image.png";
 
     @Transactional
@@ -32,23 +29,32 @@ public class UserService {
     }
 
     @Transactional
-    public String updateUserProfileImg(String userAggregateId, MultipartFile profileImg) {
-        String responseUrl = "";
-        if(profileImg != null) {
-            try {
-                responseUrl = s3FileService.upload(profileImg, "profileImg");
-            } catch (IOException e) {
-                throw new ApiException(UserMessage.PROFILE_IMAGE_UPLOAD_FAIL);
+    public String updateUserProfileImg(String userAggregateId, MultipartFile profileImg, boolean upload) {
+        String responseUrl = null;
+        if(upload) {
+            responseUrl = uploadProfileImage(profileImg);
+            if(profileImg == null) {
+                throw new ApiException(UserMessage.PROFILE_IMAGE_NOT_FOUND);
+            }
+        } else {
+            if(profileImg == null) {
+                responseUrl = DEFAULT_PROFILE_IMAGE_URL;
             }
         }
-        if(profileImg == null) {
-            responseUrl = DEFAULT_PROFILE_IMAGE_URL;
-        }
-
         commandGateway.send(new UpdateUserProfileImgCommand(
                 userAggregateId,
                 responseUrl
         ));
+        return responseUrl;
+    }
+
+    public String uploadProfileImage(MultipartFile profileImg) {
+        String responseUrl;
+        try {
+            responseUrl = s3FileService.upload(profileImg, "profileImg");
+        } catch (IOException e) {
+            throw new ApiException(UserMessage.PROFILE_IMAGE_UPLOAD_FAIL);
+        }
         return responseUrl;
     }
 }
