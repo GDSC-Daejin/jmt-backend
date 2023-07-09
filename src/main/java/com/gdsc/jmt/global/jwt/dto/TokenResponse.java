4dd04a19a8e9eb08e6d91bd17c1d066d2a7920ce
myeongju00 <j51677767@gmail.com>
2023-07-09 22:08:19 +0900

    [feature/fixLogin] feat : 회원가입, 로그인 분기처리

--- src/main/java/com/gdsc/jmt/global/jwt/dto/TokenResponse.java
+++ src/main/java/com/gdsc/jmt/global/jwt/dto/TokenResponse.java
@@ -1,9 +1,42 @@
 package com.gdsc.jmt.global.jwt.dto;
 
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+@Getter
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class TokenResponse {
+    private String grantType;
+    private String accessToken;
+    private String refreshToken;
+    private Long accessTokenExpiresIn;
+    private UserLoginAction userLoginAction;
+    public TokenResponse( String grantType,String accessToken,String refreshToken,Long accessTokenExpiresIn){
+        this.accessToken = accessToken;
+        this.grantType = grantType;
+        this.refreshToken = refreshToken;
+        this.accessTokenExpiresIn = accessTokenExpiresIn;
+        // 기본값
+        this.userLoginAction = UserLoginAction.LOG_IN;
+    }
 
-public record TokenResponse(
-         String grantType,
-         String accessToken,
-         String refreshToken,
-         Long accessTokenExpiresIn)
-{ }
+    public void updateLoginActionFlag(UserLoginAction userLoginAction){
+        this.userLoginAction = userLoginAction;
+    }
+
+    public String refreshToken() {
+        return refreshToken;
+    }
+
+    public String accessToken(){
+        return accessToken;
+    }
+
+    public Long accessTokenExpiresIn(){
+        return accessTokenExpiresIn;
+    }
+
+    public UserLoginAction userLoginAction(){
+        return userLoginAction;
+    }
+}
