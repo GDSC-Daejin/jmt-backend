774f8a6e4aa5b6515ebd3787474ac07a768f3930
myeongju00 <j51677767@gmail.com>
2023-07-22 17:02:27 +0900

    [feature/JMT-140] feat : 회원 탈퇴 API 구현

--- src/main/java/com/gdsc/jmt/global/messege/UserMessage.java
+++ src/main/java/com/gdsc/jmt/global/messege/UserMessage.java
@@ -19,7 +19,8 @@ public enum UserMessage implements ResponseMessage{
     PROFILE_IMAGE_UPDATE_SUCCESS("프로필 사진 등록에 성공하였습니다.", HttpStatus.OK),
     PROFILE_IMAGE_UPLOAD_FAIL("프로필 사진 업로드에 실패하였습니다.", HttpStatus.INTERNAL_SERVER_ERROR),
     GET_USER_SUCCESS("사용자 정보 조회에 성공하였습니다.", HttpStatus.OK),
-    PROFILE_IMAGE_NOT_FOUND("프로필 이미지가 없습니다", HttpStatus.BAD_REQUEST);
+    PROFILE_IMAGE_NOT_FOUND("프로필 이미지가 없습니다", HttpStatus.BAD_REQUEST),
+    USER_REMOVE_SUCCESS("회원 탈퇴에 성공하였습니다.", HttpStatus.OK);
 
     private final String message;
     private final HttpStatus status;
