d8f12d3979f28e8ff785997aec3b828739d7b272
boo105 <boo10500@naver.com>
2024-01-06 14:16:01 +0900

    [fix] 그룹 생성 API 구현 완료

---
+++ src/main/java/com/gdsc/jmt/domain/group/code/GroupUserRole.java
@@ -0,0 +1,6 @@
+package com.gdsc.jmt.domain.group.code;
+
+public enum GroupUserRole {
+    OWNER,  // 그룹 소유자
+    MEMBER  // 멤버
+}
--- src/main/java/com/gdsc/jmt/domain/group/command/service/GroupService.java
+++ src/main/java/com/gdsc/jmt/domain/group/command/service/GroupService.java
@@ -1,35 +1,77 @@
 package com.gdsc.jmt.domain.group.command.service;
 
+import com.gdsc.jmt.domain.group.code.GroupUserRole;
 import com.gdsc.jmt.domain.group.command.controller.request.CreateGroupRequest;
 import com.gdsc.jmt.domain.group.entity.GroupEntity;
+import com.gdsc.jmt.domain.group.entity.GroupUsersEntity;
 import com.gdsc.jmt.domain.group.repository.GroupRepository;
+import com.gdsc.jmt.domain.group.repository.GroupUserRepository;
+import com.gdsc.jmt.domain.restaurant.query.entity.RecommendRestaurantEntity;
+import com.gdsc.jmt.domain.restaurant.query.entity.RestaurantPhotoEntity;
+import com.gdsc.jmt.domain.user.query.entity.UserEntity;
+import com.gdsc.jmt.domain.user.query.repository.UserRepository;
+import com.gdsc.jmt.global.exception.ApiException;
 import com.gdsc.jmt.global.jwt.dto.UserInfo;
+import com.gdsc.jmt.global.messege.RestaurantMessage;
+import com.gdsc.jmt.global.messege.UserMessage;
+import com.gdsc.jmt.global.service.S3FileService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 @Slf4j
 @RequiredArgsConstructor
 public class GroupService {
 
+    private final UserRepository userRepository;
     private final GroupRepository groupRepository;
+    private final GroupUserRepository groupUserRepository;
+
+    private final S3FileService s3FileService;
 
     @Transactional
     public void createGroup(CreateGroupRequest request, UserInfo userInfo) {
-        GroupEntity groupEntity = GroupEntity.builder()
+        Optional<UserEntity> userResult = userRepository.findByEmail(userInfo.getEmail());
+        if(userResult.isEmpty()) {
+            throw new ApiException(UserMessage.USER_NOT_FOUND);
+        }
+        UserEntity userEntity = userResult.get();
+
+        GroupEntity.GroupEntityBuilder groupEntityBuilder = GroupEntity.builder()
                 .groupName(request.groupName)
                 .groupIntroduce(request.groupIntroduce)
-                .privateFlag(request.isPrivateGroup)
-                .build();
-        // TODO : 그룹장 세팅 (GROUP_USERS)
-        // TODO : 사진 등록
+                .privateFlag(request.isPrivateGroup);
+        this.uploadImages(groupEntityBuilder, request.getGroupProfileImage(), request.getGroupBackgroundImage());
+        GroupEntity groupEntity = groupEntityBuilder.build();
         groupRepository.save(groupEntity);
+
+        GroupUserRole ownerRole = GroupUserRole.OWNER;
+        GroupUsersEntity groupUsersEntity = GroupUsersEntity.builder()
+                .gId(groupEntity.getGid())
+                .userId(userEntity.getId())
+                .role(ownerRole)
+                .build();
+        groupUserRepository.save(groupUsersEntity);
     }
 
-    //  TODO : 만들 예정
-    private void createGroupImage() {
+    private void uploadImages(GroupEntity.GroupEntityBuilder groupEntityBuilder, MultipartFile groupProfileImage, MultipartFile groupBackgroundImage) {
+        try {
+            String groupProfileImageUrl = s3FileService.upload(groupProfileImage,"groupProfileUser");
+            String groupBackgroundImageUrl = s3FileService.upload(groupBackgroundImage,"groupBackgroundUser");
+            groupEntityBuilder.groupProfileImageUrl(groupProfileImageUrl);
+            groupEntityBuilder.groupBackgroundImageUrl(groupBackgroundImageUrl);
+        }
+        catch (IOException e) {
+            throw new ApiException(RestaurantMessage.RESTAURANT_IMAGE_UPLOAD_FAIL);
+        }
 
     }
 }
